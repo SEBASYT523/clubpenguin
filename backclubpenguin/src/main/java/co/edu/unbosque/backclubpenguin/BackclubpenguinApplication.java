52d8faee240ba7d217591eb24ef957c764f95168
SEBASYT523 <albarracinisazajuansebastian@gmail.com>
2025-04-15 14:46:53 -0500

    Back of the project
    In this commit, I send all the information about the back, SpringBoot, MongoDB and Git

---
+++ backclubpenguin/src/main/java/co/edu/unbosque/backclubpenguin/BackclubpenguinApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.backclubpenguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackclubpenguinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackclubpenguinApplication.class, args);
+	}
+
+}
---
+++ backclubpenguin/src/test/java/co/edu/unbosque/backclubpenguin/BackclubpenguinApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.backclubpenguin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackclubpenguinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
